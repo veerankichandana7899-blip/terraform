71e9ff076b767c17ac0bbc6e1dffea4cc604f74d
madhu <veeranki.chandana7899@gmail.com>
2026-01-28 16:53:22 +0530

    lambda

---
+++ day-8.1/main.tf
@@ -0,0 +1,34 @@
+
+provider "aws" {
+  
+}
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "s3_lambda" {
+  function_name = "sample"
+  role = aws_iam_role.lambda_role.arn
+  handler = "sample.lambda_handler"
+  runtime = "python3.12"
+  timeout = 900
+  memory_size = 128
+  s3_bucket = "madnhghdghdjkasds"
+  s3_key = "sample.zip"
+}
